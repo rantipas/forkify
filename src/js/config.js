2aecc93f6a3c94b1c878f83cf6a1580f23f4610a
Rommel Antipas <r_antipas@hotmail.com>
2025-12-20 19:30:30 +0300

    new forkify api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SECONDS = 20;
 export const RES_PER_PAGE = 10;
-export const API_KEY = '3da5c3f5-79da-44cc-bb5c-5f5a17ad5dd2';
+export const API_KEY = '0332fbb9-529c-4408-bfac-347bb25465ae';
